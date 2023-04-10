564a1fd3742a20eb1e440180c07c888a4c04e90a
TykeaFZ14 <tykea.20033@mhs.unesa.ac.id>
2023-04-10 15:07:21 +0700

    Update Daily Report

--- database/seeders/DailyReport.ts
+++ database/seeders/DailyReport.ts
@@ -2,29 +2,33 @@ import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import DailyReport from 'App/Models/DailyReport';
 
 export default class extends BaseSeeder {
-  public async run () {
+  public async run() {
     // Write your database queries inside the run method
     await DailyReport.createMany([
       {
+        reason: "door error",
         depletion: 100,
         feed_intake: 250,
         avg_bw: 10,
-         },
-       {
+      },
+      {
+        reason: "door error",
         depletion: 200,
         feed_intake: 20,
         avg_bw: 5,
-         },
-        {
+      },
+      {
+        reason: "door error",
         depletion: 80,
         feed_intake: 30,
         avg_bw: 8,
-         },
-         {
+      },
+      {
+        reason: "door error",
         depletion: 330,
         feed_intake: 180,
         avg_bw: 7,
-         },
-     ])
+      },
+    ])
   }
 }
