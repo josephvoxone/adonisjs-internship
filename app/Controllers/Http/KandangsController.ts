937198fd21338f3cb8dc8b0c0cb9542f21002959
Joseph <josephshandyharvian@gmail.com>
2023-04-27 23:01:27 +0700

    Update Filter Api Kandang

--- app/Controllers/Http/KandangsController.ts
+++ app/Controllers/Http/KandangsController.ts
@@ -3,64 +3,81 @@ import Kandang from 'App/Models/Kandang'
 import { DateTime } from 'luxon'
 
 export default class KandangsController {
-    /**
-     * Menampilkan semua data kandang
-     */
-    public async index({ response }: HttpContextContract) {
-        const kandang = await Kandang.query().whereNull('deleted_at').orderBy('id', 'desc')
-        return response.ok(kandang)
+  /**
+   * Menampilkan semua data kandang
+   */
+  public async index({ request, response }: HttpContextContract) {
+    const type = request.input('type')
+    const q = request.input('q')
+
+    const query = Kandang.query().whereNull('deleted_at')
+
+    if (type !== 'all') {
+      query.where('type', type)
     }
 
-    /**
+    if (q) {
+      query.where('name', 'ilike', `%${q}%`)
+    }
+
+    const kandang = await query.orderBy('id', 'desc')
+
+    return response.ok(kandang)
+  }
+
+  /**
     
     Menampilkan detail kandang berdasarkan ID
     */
-    public async show({ params, response }: HttpContextContract) {
-        const kandang = await Kandang.query().where('id', params.id).whereNull('deleted_at').firstOrFail()
-        return response.ok(kandang)
-    }
-    /**
+  public async show({ params, response }: HttpContextContract) {
+    const kandang = await Kandang.query()
+      .where('id', params.id)
+      .whereNull('deleted_at')
+      .firstOrFail()
+    return response.ok(kandang)
+  }
+  /**
     
     Membuat data kandang baru
     */
-    public async store({ request, response }: HttpContextContract) {
-        const data = request.only(['name', 'type', 'status', 'address', 'city'])
-        const kandang = new Kandang()
-        kandang.name = data.name
-        kandang.type = data.type
-        kandang.status = data.status
-        kandang.address = data.address
-        kandang.city = data.city
-        await kandang.save()
+  public async store({ request, response }: HttpContextContract) {
+    const data = request.only(['name', 'type', 'status', 'address', 'city'])
+    const kandang = new Kandang()
+    kandang.name = data.name
+    kandang.type = data.type
+    kandang.status = data.status
+    kandang.address = data.address
+    kandang.city = data.city
+    await kandang.save()
 
-        return response.created(kandang)
-    }
+    return response.created(kandang)
+  }
 
-    /**
+  /**
     
     Mengupdate data kandang berdasarkan ID
     */
-    public async update({ params, request, response }: HttpContextContract) {
-        const data = request.only(['name', 'type', 'status', 'address', 'city'])
-        const kandang = await Kandang.findOrFail(params.id)
-        kandang.name = data.name
-        kandang.type = data.type
-        kandang.status = data.status
-        kandang.address = data.address
-        kandang.city = data.city
-        await kandang.save()
+  public async update({ params, request, response }: HttpContextContract) {
+    const data = request.only(['name', 'type', 'status', 'address', 'city'])
+    const kandang = await Kandang.findOrFail(params.id)
+    kandang.name = data.name
+    kandang.type = data.type
+    kandang.status = data.status
+    kandang.address = data.address
+    kandang.city = data.city
+    await kandang.save()
 
-        return response.ok(kandang)
-    }
+    return response.ok(kandang)
+  }
 
-    /**
+  /**
     
     Menghapus data kandang berdasarkan ID (soft delete)
     */
-    public async destroy({ params, response }: HttpContextContract) {
-        const kandang = await Kandang.findOrFail(params.id)
-        kandang.deleted_at = DateTime.local()
-        await kandang.save()
-        return response.noContent()
-    }
+  public async destroy({ params, response }: HttpContextContract) {
+    const kandang = await Kandang.findOrFail(params.id)
+    kandang.deleted_at = DateTime.local()
+    await kandang.save()
+    return response.noContent()
+  }
 }
